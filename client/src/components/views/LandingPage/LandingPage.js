b4c9e5356a2cb8fe549b470ebc480bacd84a6368
tintoll <belselios@gmail.com>
2020-05-14 06:11:37 +0900

    로그아웃 추가

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,11 +1,24 @@
 import React, {useEffect} from 'react'
 import axios from 'axios';
 
-function LandingPage() {
+function LandingPage(props) {
   useEffect(() => {
     axios.get('/api/hello')
     .then(response => console.log(response.data))
   },[])
+
+  const onClickHandler = () => {
+    
+    // redux의 값을 변경하지 않으려면 바로 axios 로 호출한다. 
+    axios.get('/api/users/logout')
+      .then(response => {
+        if(response.data.success) {
+          props.history.push('/login')
+        } else {
+          alert('로그아웃 실패')
+        }
+      })
+  }
 
   return (
     <div style={{
@@ -13,6 +26,9 @@ function LandingPage() {
       width : '100%', height : '100vh'
     }}>
       <h2>시작페이지</h2>
+      <button onClick={onClickHandler}>
+        로그아웃
+      </button>
     </div>
   )
 }
